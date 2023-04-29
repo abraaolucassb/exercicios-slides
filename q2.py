6dc8e92a580a3b60732a8e13acc94d4123fb2d4b
Abraão Lucas <abraaolucassb@gmail.com>
2023-04-29 14:50:04 -0300

    estudando map()

--- q2.py
+++ q2.py
@@ -6,27 +6,42 @@ Explique a função map()
 # map(function, iterable, *iterables)
 
 '''
-    Return an iterator that applies function to every item of iterable, yielding the results. 
-    If additional iterables arguments are passed, function must take that many arguments and is 
-    applied to the items from all iterables in parallel. With multiple iterables, the iterator 
-    stops when the shortest iterable is exhausted. For cases where the function inputs are already 
-    arranged into argument tuples, see itertools.starmap().
+    Retorna um iterador que aplica a função a cada item iterável, gerando os resultados.
+    Se argumentos adicionais de iteráveis forem passados, a função deve receber a mesma quantidade 
+    de argumentos e será aplicada aos itens de todos os iteráveis em paralelo. Com múltiplos iteráveis, 
+    o iterador para quando o iterável mais curto é esgotado.
 
+    Em outras palavras, map() executa uma operação em cada item de um iterável e retorna um novo 
+    iterável com os resultados dessa operação.
 '''
 
+# temos aqui um exemplo de uma função para adicionar um imposto sobre um preço de uma lista
 
-# itertools.starmap(function, iterable)
+precos = [1000, 1500, 1250, 2500]
 
-'''
-    Make an iterator that computes the function using arguments obtained from the iterable. 
-    Used instead of map() when argument parameters are already grouped in tuples from a single 
-    iterable (when the data has been “pre-zipped”).
 
-    The difference between map() and starmap() parallels the distinction between function(a,b) 
-    and function(*c). Roughly equivalent to:
+def adicionar_imposto(preco):
+    return preco * 1.1
 
-    def starmap(function, iterable):
-        # starmap(pow, [(2,5), (3,2), (10,3)]) --> 32 9 1000
-        for args in iterable:
-            yield function(*args)
-'''
+# mas e se eu quiser adicionar o imposto para todos os itens de uma lista ao mesmo tempo?
+# EXEMPLO FOR:
+
+
+# precos_com_imposto = []
+
+# for preco in precos:
+    # precos_com_imposto.append(adicionar_imposto(preco))
+
+# print(precos_com_imposto)
+
+# # EXEMPLO COM MAP:
+
+precos_com_imposto2 = list(map(adicionar_imposto, precos))
+
+print(precos_com_imposto2)
+
+# esse código vai retornar isso: <map object at 0x7fefa62ff640>
+# para fazer o map retornar em formato de lista basta colocar list(map(...))
+# após isso vou ter como retorno: [1100.0, 1650.0000000000002, 1375.0, 2750.0]
+# basicamente o map() tá pegando um item da lista preços e passando como parâmetro para a...
+# ...função adicionar_imposto
