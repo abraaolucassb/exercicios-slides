b2d499be083741b28756ec2bb294ca8de5641545
Abraão Lucas <abraaolucassb@gmail.com>
2023-04-30 18:55:29 -0300

    melhorando o estudo do match

--- q5.py
+++ q5.py
@@ -52,3 +52,24 @@ def ponto(point):
 
 
 ponto((9, 4))
+
+
+dia = 1
+
+match dia:
+    case 1:
+        print('Domingo')
+    case 2:
+        print('Segunda')
+    case 3:
+        print('Terça')
+    case 4:
+        print('Quarta')
+    case 5:
+        print('Quinta')
+    case 6:
+        print('Sexta')
+    case 7:
+        print('Sábado')
+    case _:
+        print('Dia inválido')
